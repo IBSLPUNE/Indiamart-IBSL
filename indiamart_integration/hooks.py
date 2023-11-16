2bd456ad6288bd0ef67996d8e0e45a5f76bbf00a
IBSLPUNE <31270467+IBSLPUNE@users.noreply.github.com>
2023-11-16 18:11:40 +0530

    Update hooks.py

--- indiamart_integration/hooks.py
+++ indiamart_integration/hooks.py
@@ -3,12 +3,12 @@ from __future__ import unicode_literals
 from . import __version__ as app_version
 
 app_name = "indiamart_integration"
-app_title = "Indiamart Integration"
-app_publisher = "Sagar Ratan Garg"
+app_title = "Indiamart Integration "
+app_publisher = ""
 app_description = "Indiamart Integration"
 app_icon = "octicon octicon-file-directory"
 app_color = "grey"
-app_email = "sagar1ratan1garg1@gmail.com"
+app_email = ""
 app_license = "MIT"
 
 # Includes in <head>
